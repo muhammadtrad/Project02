35a14f5acb0fa22fcef0b04a1f8bf57fb3c7ad04
xmagnetisx <muhammad.trad@yahoo.com>
2020-05-02 23:10:58 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+
+const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/user");
+const bodyParser = require('body-parser');
+
+
+mongoose 
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World"));
+app.use("/api/users", users);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on ${port}`));
+
+
+
