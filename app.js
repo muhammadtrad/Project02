8923679f7db66b30bfe5b382ed1f9dae6c48852e
xmagnetisx <muhammad.trad@yahoo.com>
2020-05-03 22:33:59 -0700

    add routes

--- app.js
+++ app.js
@@ -3,22 +3,34 @@ const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
-const users = require("./routes/api/user");
+const users = require("./routes/api/users");
 const bodyParser = require('body-parser');
+const User = require('./models/User');
 
 
-mongoose 
+mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
-app.use("/api/users", users);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
+// app.get("/", (req, res) => {
 
+//     const user = new User({
+//         handle: "jim",
+//         email: "jim@jim.jim",
+//         password: "jimisgreat123"
+//     });
+//     user.save();
+
+//     res.send("Hello World")
+// }
+// );
+
+app.use("/users", users);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on ${port}`));
