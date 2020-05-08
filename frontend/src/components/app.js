3d7e054528ad2677d2038688d1ca16bd98899dc4
xmagnetisx <muhammad.trad@yahoo.com>
2020-05-07 23:23:31 -0700

    add routes

---
+++ frontend/src/components/app.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import { Switch } from 'react-router-dom';
+
--- frontend/src/util/route_util.js
+++ frontend/src/util/route_util.js
@@ -3,5 +3,36 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect, withRouter } from 'react-router-dom';
 
+//Passed in through parent component or from mapStateToProps
+//Directs user to component or home page depending on user login state
 const Auth = ({ component: Component, path, loggedIn, exact }) => (
+    <Route path={path} exact={exact} render={ (props) => (
+        !loggedIn ? (
+            <Component {...props} />
+        ) : (
+            <Redirect to="/home" /> 
+        )
+    )} />
+);
 
+//Redirect user to login page if not logged in
+const Protected = ({ component: Component, loggedIn, ...rest }) => (
+    <Route 
+        {...rest}
+        render= {props =>
+            loggedIn ? (
+                <Component {...props} />
+            ) : (
+                <Redirect to="/login" />
+            )
+            }
+        />
+);
+
+const mapStateToProps = state => (
+    {loggedIn: state.session.isAuthenticated}
+);
+
+export const AuthRoute = withRouter(connect(mapStateToProps)(Auth));
+
+export const ProtectedRoute = withRouter(connect(mapStateToProps)(Protected));
