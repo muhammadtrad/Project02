18f8d59cb1a34f50068e2bdbcd197c4b8dec91a8
xmagnetisx <muhammad.trad@yahoo.com>
2020-05-22 18:57:44 -0700

    configure server

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const bodyParser = require('body-parser');
 const User = require('./models/User');
-
+const path = require('path');
 
 mongoose
     .connect(db, { useNewUrlParser: true })
@@ -36,4 +36,9 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on ${port}`));
 
 
-
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
